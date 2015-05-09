56e5d3a67fdaab35c9c97fd7c1b2775e23f87ab1
Jefferson Silva (Windows Notebook) <jefferson.jslopes@gmail.com>
2015-05-09 20:49:35 -0300

    implement updatediary.php

---
+++ updatediary.php
@@ -0,0 +1,16 @@
+<?php
+	include ("connection.php");
+
+	session_start ();
+
+	if (isset($_SESSION["id"]) and isset($_POST["update"])) {
+
+		$diary = mysqli_real_escape_string ($connection, $_POST["diary"]);
+		$id = $_SESSION["id"];
+
+		$query = "UPDATE users SET diary='$diary' WHERE id='$id' LIMIT 1";
+
+		mysqli_query ($connection, $query);
+
+	}
+?>
